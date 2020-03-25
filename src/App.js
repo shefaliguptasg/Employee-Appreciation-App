869e3ef249c9a515e35aa6c42d3c0edc66b10bd8
Shefali Gupta <shefaligupta2may@gmail.com>
2020-03-25 12:48:18 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -9,25 +9,3 @@ const App = () => (
 );
 export default App;
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//          Hello Word
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-//export default App;
